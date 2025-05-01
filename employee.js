c9d7504b6bfcee2b47439cfa4dfcbfd752bfd9ae
Eli_Mandujano <elite3210@gmail.com>
2025-04-30 22:48:27 -0500

    RED: se crea una prueba que fallará

---
+++ employee.js
@@ -0,0 +1,10 @@
+console.log('Dentro del script empleados.js...');
+
+function calcularPago(tarifaPorHora, horasTrabajadas) {
+    // Todavía no implementamos la lógica correctamente
+    return 0; // Sabemos que esto hará que la prueba falle
+}
+
+function calcularPagoExtra(tarifaPorHora, horasExtras) {
+    return horasExtras * tarifaPorHora * 1.5;
+}
