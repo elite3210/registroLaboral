f50ab01795f2717fde7fd52e40c7082d7d73a076
Eli_Mandujano <elite3210@gmail.com>
2025-04-30 23:22:18 -0500

    Refactor: se implemento una segunda funcion y se utilizo en la funcion calcularPago()

--- employee.js
+++ employee.js
@@ -1,14 +1,13 @@
 console.log('Dentro del script empleados.js...');
 
+function calcularPagoExtra(tarifaPorHora, horasExtras) {
+    return horasExtras * tarifaPorHora * 1.5;
+}
+
 function calcularPago(tarifaPorHora, horasTrabajadas) {
     const horasNormales = Math.min(horasTrabajadas, 8);
     const horasExtras = Math.max(0, horasTrabajadas - 8);
     const pagoNormal = horasNormales * tarifaPorHora;
-    const pagoExtra = horasExtras * tarifaPorHora * 1.5;
+    const pagoExtra = calcularPagoExtra(tarifaPorHora, horasExtras);
     return pagoNormal + pagoExtra;
-}
-
-function calcularPagoExtra(tarifaPorHora, horasExtras) {
-    //implementado el pago de horas extras, segun ley se paga 50% mas
-    return horasExtras * tarifaPorHora * 1.5;
 }
