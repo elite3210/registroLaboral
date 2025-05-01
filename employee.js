0532def97a7f9b6b2e0914a15bbea45382723c20
Eli_Mandujano <elite3210@gmail.com>
2025-04-30 23:19:23 -0500

    Geen: se implemnto el calculo de la horas extras kata2

--- employee.js
+++ employee.js
@@ -1,9 +1,14 @@
 console.log('Dentro del script empleados.js...');
 
 function calcularPago(tarifaPorHora, horasTrabajadas) {
-    return tarifaPorHora * horasTrabajadas;
+    const horasNormales = Math.min(horasTrabajadas, 8);
+    const horasExtras = Math.max(0, horasTrabajadas - 8);
+    const pagoNormal = horasNormales * tarifaPorHora;
+    const pagoExtra = horasExtras * tarifaPorHora * 1.5;
+    return pagoNormal + pagoExtra;
 }
 
 function calcularPagoExtra(tarifaPorHora, horasExtras) {
-    return horasExtras * tarifaPorHora;
+    //implementado el pago de horas extras, segun ley se paga 50% mas
+    return horasExtras * tarifaPorHora * 1.5;
 }
