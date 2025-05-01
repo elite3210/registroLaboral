f84e6098ec539e8238880c793bb40ea11c873131
Eli_Mandujano <elite3210@gmail.com>
2025-04-30 23:30:37 -0500

    Refactor: se implemento la funcion validarTarifaPorHora() para evitar numero negativos

--- employee.js
+++ employee.js
@@ -1,13 +1,17 @@
 console.log('Dentro del script empleados.js...');
 
+function validarTarifaPorHora(tarifaPorHora) {
+    if (tarifaPorHora < 0) {
+        throw new Error('La tarifa por hora no puede ser negativa.');
+    }
+}
+
 function calcularPagoExtra(tarifaPorHora, horasExtras) {
     return horasExtras * tarifaPorHora * 1.5;
 }
 
 function calcularPago(tarifaPorHora, horasTrabajadas) {
-    if (tarifaPorHora < 0) {
-        throw new Error('La tarifa por hora no puede ser negativa.');
-    }
+    validarTarifaPorHora(tarifaPorHora);
     const horasNormales = Math.min(horasTrabajadas, 8);
     const horasExtras = Math.max(0, horasTrabajadas - 8);
     const pagoNormal = horasNormales * tarifaPorHora;
